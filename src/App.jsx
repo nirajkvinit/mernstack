8596d00f2a7fe7bdca2d84f62d7faf32aaef2d37
Niraj Kumar <nirajkvinit@gmail.com>
2018-06-29 12:32:56 +0530

    Ch2 part one coding complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,72 @@
 const contentNode = document.getElementById('contents');
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
-const component = <p>{message}</p>;
-ReactDOM.render(component, contentNode);
+
+class IssueFilter extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the Issue Filter.</div>
+        );
+    }
+}
+
+class IssueRow extends React.Component {    
+    render() {
+        const borderedStyle = {border: "1px solid silver", padding: 4};
+        return (
+            <tr>
+                <td style={borderedStyle}>{this.props.issue_id}</td>
+                <td style={borderedStyle}>{this.props.issue_title}</td>
+            </tr>
+        );
+    }
+}
+
+class IssueTable extends React.Component {
+    render() {
+        const borderedStyle = {border: "1px solid silver", padding: 6};
+        return (
+            <table style={{borderCollapse: "collapse"}}>
+                <thead>
+                    <tr>
+                        <th style={borderedStyle}>Id</th>
+                        <th style={borderedStyle}>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow 
+                        issue_id={1}
+                        issue_title="Error in console when clicking Add"
+                    />
+                    <IssueRow 
+                        issue_id={2}
+                        issue_title="Missing bottom border on panel"
+                    />
+                </tbody>
+            </table>
+        );
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for an Issue Add entry form.</div>
+        );
+    }
+}
+
+class IssueList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<IssueList />, contentNode);
