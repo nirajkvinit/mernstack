68ec79feeddd3ac0425a151be81c3144bd8db0cc
Niraj Kumar <nirajkvinit@gmail.com>
2018-07-03 11:37:24 +0530

    Webpack and react component modularization

---
+++ src/IssueFilter.jsx
@@ -0,0 +1,7 @@
+export default class IssueFilter extends React.Component {
+    render() {
+        return (
+            <div>This is a test placeholeder for the Issue Filter</div>
+        );
+    }
+}
