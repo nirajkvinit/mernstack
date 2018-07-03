26b4c8b9609424141a12b8a89938253e8cd9141d
Niraj Kumar <nirajkvinit@gmail.com>
2018-07-03 16:42:37 +0530

    Code complete

--- src/IssueFilter.jsx
+++ src/IssueFilter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-export default class IssueFilter extends React.Component {
+export default class IssueFilter extends React.Component { // eslint-disable-line max-len,react/prefer-stateless-function
     render() {
         return (
             <div>This is a placeholder for the Issue Filter</div>
