b22782b8306ffec87e66aa504071d69a786c63df
Niraj Kumar <nirajkvinit@gmail.com>
2018-07-05 16:12:01 +0530

    Issue Fetching API

--- server/server.js
+++ server/server.js
@@ -15,6 +15,30 @@ const app = express();
 app.use(express.static('static'));
 app.use(bodyParser.json());
 
+app.get('/api/issues/:id', (req, res) => {
+    let issueId;
+    try {
+        issueId = new ObjectId(req.params.id);
+    } catch (error) {
+        res.status(422).json({ message: `Invalid issue ID format: ${error}` });
+        return;
+    }
+
+    db.collection('issues').find({ _id: issueId }).limit(1)
+    .next()
+    .then(issue => {
+        if (!issue) {
+            res.status(422).json({ message: `No such issue: ${issueId}` });
+        } else {
+            res.json(issue);
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: `Internal Server Error: ${error}` });
+    });
+});
+
 app.get('/api/issues', (req, res) => {
     const filter = {};
 
