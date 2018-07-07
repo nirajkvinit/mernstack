542f9e6a0486df6b6a8f77075c224756b4175246
Niraj Kumar <nirajkvinit@gmail.com>
2018-07-07 16:03:58 +0530

    Edit form updated

--- src/IssueEdit.jsx
+++ src/IssueEdit.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router';
+
+import { FormGroup, FormControl, ControlLabel, ButtonToolbar, Button, Panel, Form, Col } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
 import NumInput from './NumInput.jsx';
 import DateInput from './DateInput.jsx';
 
@@ -108,43 +111,79 @@ export default class IssueEdit extends React.Component { // eslint-disable-line
             === 0 ? null
             : (<div className="error">Please correct invalid fields before submitting.</div>);
         return (
-            <div>
-                <form onSubmit={this.onSubmit}>
-                    ID: {issue._id}
-                    <br />
-                    Created: {issue.created ? issue.created.toDateString() : ''}
-                    <br />
-                    Status:
-                    &nbsp;<select name="status" value={issue.status} onChange={this.onChange}>
-                        <option value="New">New</option>
-                        <option value="Open">Open</option>
-                        <option value="Assigned">Assigned</option>
-                        <option value="Fixed">Fixed</option>
-                        <option value="Verified">Verified</option>
-                        <option value="Closed">Closed</option>
-                    </select>
-                    <br />
-                    Owner: <input name="owner" value={issue.owner} onChange={this.onChange} />
-                    <br />
-                    Effort: <NumInput size={5} name="effort" value={issue.effort} onChange={this.onChange} />
-                    <br />
-                    Completion Date:
-                    &nbsp;<DateInput
-                        name="completionDate" value={issue.completionDate} onChange={this.onChange}
-                        onValidityChange={this.onValidityChange}
-                    />
-                    <br />
-                    Title:
-                    &nbsp;<input
-                        name="title"
-                        size={50} value={issue.title} onChange={this.onChange}
-                    />
-                    <br />
-                    {validationMessage}
-                    <button type="submit">Submit</button>
-                    &nbsp;<Link to="/issues">Back to issue list</Link>
-                </form>
-            </div>
+            <Panel header="Edit Issue">
+                <Form horizontal onSubmit={this.onSubmit}>
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={3}>ID</Col>
+                        <Col sm={9}>
+                            <FormControl.Static>{issue._id}</FormControl.Static>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={3}>Created</Col>
+                        <Col sm={9}>
+                            <FormControl.Static>
+                                {issue.created ? issue.created.toDateString() : ''}
+                            </FormControl.Static>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={3}>Status</Col>
+                        <Col sm={9}>
+                            <FormControl componentClass="select" name="status" value={issue.status} onChange={this.onChange}>
+                                <option value="New">New</option>
+                                <option value="Open">Open</option>
+                                <option value="Assigned">Assigned</option>
+                                <option value="Fixed">Fixed</option>
+                                <option value="Verified">Verified</option>
+                                <option value="Closed">Closed</option>
+                            </FormControl>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={3}>Owner</Col>
+                        <Col sm={9}>
+                            <FormControl name="owner" value={issue.owner} onChange={this.onChange} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={3}>Effort</Col>
+                        <Col sm={9}>
+                            <FormControl componentClass={NumInput} name="effort" value={issue.effort} onChange={this.onChange} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup validationState={this.state.invalidFields.completionDate ? 'error' : null}>
+                        <Col componentClass={ControlLabel} sm={3}>Completion Date</Col>
+                        <Col sm={9}>
+                            <FormControl
+                                componentClass={DateInput}
+                                name="completionDate"
+                                value={issue.completionDate}
+                                onChange={this.onChange}
+                                onValidityChange={this.onValidityChange}
+                            />
+                            <FormControl.Feedback />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={3}>Title</Col>
+                        <Col sm={9}>
+                            <FormControl name="title" value={issue.title} onChange={this.onChange} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup>
+                        <Col smOffset={3} sm={6}>
+                            <ButtonToolbar>
+                                <Button bsStyle="primary" type="submit">Submit</Button>
+                                <LinkContainer to="/issues">
+                                    <Button bsStyle="link">Back</Button>
+                                </LinkContainer>
+                            </ButtonToolbar>
+                        </Col>
+                    </FormGroup>
+                </Form>
+                {validationMessage}
+            </Panel>
         );
     }
 }
